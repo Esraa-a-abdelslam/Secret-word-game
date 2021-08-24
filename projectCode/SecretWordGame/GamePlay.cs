1f22bfe671c28d6d7a5e8082c1bef32d5a972b84
Esraa-a-abdelslam <esraa.a.abdelslam@gmail.com>
2021-08-24 20:59:40 +0200

    project

---
+++ projectCode/SecretWordGame/GamePlay.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Threading;
+using System.Windows.Forms;
+using Business;
+
+namespace SecretWordGame
+{
+    public partial class GamePlay : Form
+    {
+        string secretWord;
+        string difficulty;
+        string category;
+        bool formIsClosing = false;
+        bool clientDisconnected = false;
+        int serverResult, clientResult;
+        List<char> pressedKeys;
+        //Network network;
+        NetworkServices network;
+        UIService ui;
+
+        SoundPlayer simpleSound;
+
+        public string Difficulty
+        {
+            get
+            {
+                return difficulty;
+            }
+            set
+            {
+                difficulty = value;
+                lblDifficulty.Text = difficulty;
+            }
+        }
+
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+            set
+            {
+                category = value;
+                lblCategory.Text = category;
+            }
+        }
+
+        public GamePlay(NetworkServices network)
+        {
+            InitializeComponent();
+            serverResult = clientResult = 0;
+            this.network = network;
+            pressedKeys = new List<char>();
+            ui = new UIService();
+            network.ServerStoped += NetworkServerStoped;
+            network.ClientPressedLetter += NetworkClientPressedLetter;
+            network.ClientPlayAgainResponse += NetworkClientPlayAgainResponse;
+
+            simpleSound = new SoundPlayer(@"tick.wav");
+
+            ui.DrawKeyBoard(this, LetterClick);
+        }
+
+        private void NetworkClientPlayAgainResponse(object sender, PlayAgainArgs e)
+        {
+            if (e.Response == "yes")
+            {
+                NewGame();
+            }
+            else
+            {
+                MessageBox.Show("Client refused to play again", "Game ended", MessageBoxButtons.OK);
+                network.Stop();
+            }
+        }
+
+        private void NetworkServerStoped(object sender, EventArgs e)
+        {
+            network.ServerStoped -= NetworkServerStoped;
+            clientDisconnected = true;
+            if (!formIsClosing)
+            {
+                this.Invoke((MethodInvoker)delegate ()
+                {
+                    this.Close();
+                });
+            }
+        }
+
+        private void GamePlayFormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (clientDisconnected)
+            {
+                SaveResultsToFile();
+                MessageBox.Show($"Client Dissconnected\nResult\nServer: {serverResult} #### Client: {clientResult}", "Game Ended", MessageBoxButtons.OK);
+            }
+            else
+            {
+                var res = MessageBox.Show("Are you sure you want to Exit?", "Server", MessageBoxButtons.YesNo);
+                if (res == DialogResult.No)
+                {
+                    e.Cancel = true;
+                }
+                else
+                {
+                    SaveResultsToFile();
+                    MessageBox.Show($"Result\n\n\tServer: {serverResult} \n\t Client: {clientResult}", "Game Ended", MessageBoxButtons.OK);
+
+                    formIsClosing = true;
+                    network.Stop();
+                }
+            }
+        }
+
+        private void SaveResultsToFile()
+        {
+            try
+            {
+                StreamWriter writer = File.AppendText("Results.txt");
+                writer.WriteLine($"Server: {serverResult} : Client: {clientResult}");
+                writer.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.StackTrace);
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void LetterClick(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.Enabled = false;
+
+            char letter = btn.Text[0];
+            pressedKeys.Add(letter);
+            network.Send("letter", letter.ToString());
+
+            if (Guess(letter))
+            {
+                if (CheckFinshed())
+                {
+                    // server win
+                    //MessageBox.Show("Server Win");
+                    ++serverResult;
+
+                    simpleSound.SoundLocation = "end.wav";
+                    simpleSound.Play();
+                    EnableKeyBoard(false);
+                    DialogResult result = MessageBox.Show("Do you want to play Again?", "play again", MessageBoxButtons.YesNo);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        network.Send("playAgain", "yes");
+                    }
+                    else
+                    {
+                        network.Send("playAgain", "no");
+                        Thread.Sleep(500);
+                        network.Stop();
+                    }
+                }
+                else
+                {
+                    simpleSound.SoundLocation = "tick.wav";
+                    simpleSound.Play();
+                }
+            }
+            else
+            {
+                simpleSound.SoundLocation = "tick.wav";
+                simpleSound.Play();
+                EnableKeyBoard(false);
+            }
+        }
+
+        private void NetworkClientPressedLetter(object sender, LetterPressedArgs e)
+        {
+            char letter = e.Letter;
+            pressedKeys.Add(letter);
+
+            if (Guess(letter))
+            {
+                if (CheckFinshed())
+                {
+                    // client win
+                    //MessageBox.Show("Client Win");
+                    ++clientResult;
+
+                    simpleSound.SoundLocation = "end.wav";
+                    simpleSound.Play();
+
+                    EnableKeyBoard(false);
+                    DialogResult result = MessageBox.Show("Do you want to play Again?", "play again", MessageBoxButtons.YesNo);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        network.Send("playAgain", "yes");
+                    }
+                    else
+                    {
+                        network.Send("playAgain", "no");
+                        Thread.Sleep(500);
+                        network.Stop();
+                    }
+
+                }
+                else
+                {
+                    simpleSound.SoundLocation = "tick.wav";
+                    simpleSound.Play();
+                }
+            }
+            else
+            {
+                simpleSound.SoundLocation = "tick.wav";
+                simpleSound.Play();
+                EnableKeyBoard(true);
+            }
+        }
+
+        private bool Guess(char letter)
+        {
+            bool match = false;
+            pressedKeys.Add(letter);
+
+            if (secretWord.ToUpper().Contains(letter))
+            {
+                int index = secretWord.ToUpper().IndexOf(letter);
+                while (index != -1)
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        (this.Controls["lettersPanel"].Controls[index] as Label).Text = letter.ToString();
+                    });
+                    index = secretWord.ToUpper().IndexOf(letter, index + 1);
+                }
+
+                match = true;
+            }
+
+            return match;
+        }
+
+        private bool CheckFinshed()
+        {
+            bool finished = true;
+            for (int i = 0; i < secretWord.Length; ++i)
+            {
+                if ((this.Controls["lettersPanel"].Controls[i] as Label).Text == "_")
+                {
+                    finished = false;
+                }
+            }
+            return finished;
+        }
+
+        private void NewGame()
+        {
+            CFDB ENT = new CFDB();
+            var Sec = (from S in ENT.WordsGames
+                       where S.Difficulty == this.Difficulty
+                            && S.Category == this.category
+                       orderby Guid.NewGuid()
+                       select S.Word).FirstOrDefault();
+            secretWord = Sec;
+            pressedKeys.Clear();
+
+            network.Send("newGame", secretWord);
+            this.Invoke((MethodInvoker)delegate ()
+            {
+                this.lblClientResult.Text = clientResult.ToString();
+                this.lblServerResult.Text = serverResult.ToString();
+                ui.DrawWord(this, secretWord);
+            });
+
+            EnableKeyBoard(true);
+        }
+
+        private void EnableKeyBoard(bool enable)
+        {
+            foreach (Control c in this.Controls["keyBoardPanel"].Controls)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    if (enable && !pressedKeys.Contains(c.Text[0]))
+                    {
+                        c.Enabled = true;
+                    }
+                    else
+                    {
+                        c.Enabled = false;
+                    }
+                });
+            }
+        }
+
+        private void GamePlayFormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.OpenForms["Form1"].Show();
+        }
+
+        private void GamePlayLoad(object sender, EventArgs e)
+        {
+            Application.OpenForms["Form1"].Hide();
+            NewGame();
+        }
+    }
+}
---
+++ projectCode/SecretWordGameClient/GamePlay.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Linq;
+using Business;
+
+namespace SecretWordGameClient
+{
+    public partial class GamePlay : Form
+    {
+        string secretWord;
+
+        List<char> pressedKeys;
+        ClientNetworkServices network;
+        bool serverDisconnected = false;
+        int serverResult, clientResult;
+        UIService ui;
+
+        public GamePlay(ClientNetworkServices network)
+        {
+            InitializeComponent();
+
+            serverResult = clientResult = 0;
+            this.network = network;
+            pressedKeys = new List<char>();
+            ui = new UIService();
+            network.Disconnected += NetworkDisconnected;
+            network.ServerPressedLetter += NetworkServerPressedLetter;
+            network.NewGame += NetworkNewGame;
+            network.playagain += Networkplayagain;
+
+            ui.DrawKeyBoard(this, LetterClick);
+        }
+
+        private void Networkplayagain(object sender, PlayAgainArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate ()
+            {
+                progressBar1.Visible = false;
+            });
+
+            if (e.Response == "no")
+            {
+                MessageBox.Show("Server refused to play again", "Game ended", MessageBoxButtons.OK);
+
+                network.Stop();
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("Do you want to play Again?", "play again", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    network.Send("playAgain", "yes");
+                }
+                else
+                {
+                    network.Send("playAgain", "no");
+                    network.Stop();
+                }
+            }
+        }
+
+        private void NetworkNewGame(object sender, Business.NewGamePressedArgs e)
+        {
+            secretWord = e.SecretWord;
+            pressedKeys.Clear();
+
+            //Invalidate();
+            if (this.InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)delegate ()
+                 {
+                     this.lblClientResult.Text = clientResult.ToString();
+                     this.lblServerResult.Text = serverResult.ToString();
+                     ui.DrawWord(this, secretWord);
+                 });
+            }
+            else
+            {
+                ui.DrawWord(this, secretWord);
+            }
+
+            EnableKeyBoard(false);
+        }
+
+        private void NetworkServerPressedLetter(object sender, Business.LetterPressedArgs e)
+        {
+            char letter = e.Letter;
+            pressedKeys.Add(letter);
+
+            if (Guess(letter))
+            {
+                if (CheckFinshed())
+                {
+                    // server win
+                    ++serverResult;
+                    //MessageBox.Show("Server Win", "Client");
+                    this.Invoke((MethodInvoker)delegate ()
+                    {
+                        progressBar1.Visible = true;
+                        EnableKeyBoard(false);
+                    });
+                }
+            }
+            else
+            {
+                EnableKeyBoard(true);
+            }
+        }
+
+        private void NetworkDisconnected(object sender, System.EventArgs e)
+        {
+
+            network.Disconnected -= NetworkDisconnected;
+            network.ServerPressedLetter -= NetworkServerPressedLetter;
+            network.NewGame -= NetworkNewGame;
+            serverDisconnected = true;
+            // MessageBox.Show("Game Ended");
+            try
+            {
+                this.Invoke((MethodInvoker)delegate ()
+                {
+                    this.Close();
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                this.Close();
+            }
+        }
+
+        private void GamePlayFormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serverDisconnected)
+            {
+                MessageBox.Show($"Server: {serverResult}<==> Client: {clientResult}", "Game Ended", MessageBoxButtons.OK);
+            }
+            else
+            {
+                var res = MessageBox.Show("Are you sure you want to Exit?", "Client", MessageBoxButtons.YesNo);
+                if (res == DialogResult.No)
+                {
+                    e.Cancel = true;
+                }
+                else
+                {
+                    MessageBox.Show($"Server: {serverResult} \n\t Client: {clientResult}", "Game Ended", MessageBoxButtons.OK);
+
+                    network.Disconnected -= NetworkDisconnected;
+                    network.ServerPressedLetter -= NetworkServerPressedLetter;
+                    network.NewGame -= NetworkNewGame;
+
+                    network.Stop();
+                }
+            }
+        }
+
+        private void LetterClick(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.Enabled = false;
+
+            char letter = btn.Text[0];
+            pressedKeys.Add(letter);
+            network.Send("letter", letter.ToString());
+
+            if (Guess(letter))
+            {
+                if (CheckFinshed())
+                {
+                    ++clientResult;
+                    progressBar1.Visible = true;
+                    EnableKeyBoard(false);
+                }
+            }
+            else
+            {
+                this.Invoke((MethodInvoker)delegate ()
+                {
+                    EnableKeyBoard(false);
+                });
+            }
+        }
+
+        private bool Guess(char letter)
+        {
+            bool match = false;
+            pressedKeys.Add(letter);
+
+            if (secretWord.ToUpper().Contains(letter))
+            {
+                int index = secretWord.ToUpper().IndexOf(letter);
+                while (index != -1)
+                {
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        (this.Controls["lettersPanel"].Controls[index] as Label).Text = letter.ToString();
+                    });
+                    index = secretWord.ToUpper().IndexOf(letter, index + 1);
+                }
+
+                match = true;
+            }
+
+            return match;
+        }
+
+        private bool CheckFinshed()
+        {
+            bool finished = true;
+            for (int i = 0; i < secretWord.Length; ++i)
+            {
+                if ((this.Controls["lettersPanel"].Controls[i] as Label).Text == "_")
+                {
+                    finished = false;
+                }
+            }
+
+            return finished;
+        }
+
+        private void GamePlay_Load(object sender, EventArgs e)
+        {
+            Application.OpenForms["Form1"].Hide();
+        }
+
+        private void GamePlay_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.OpenForms["Form1"].Show();
+        }
+
+        private void EnableKeyBoard(bool enable)
+        {
+            if (!IsHandleCreated)
+                this.CreateControl();
+            foreach (Control c in this.Controls["keyBoardPanel"].Controls)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    if (enable && !pressedKeys.Contains(c.Text[0]))
+                    {
+                        c.Enabled = true;
+                    }
+                    else
+                    {
+                        c.Enabled = false;
+                    }
+                });
+            }
+        }
+    }
+}
